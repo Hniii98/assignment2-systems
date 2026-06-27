327fd28a42433c52be1cd4cfa3801935bd0e95fc
Hniii98 <752608928@qq.com>
2026-06-27 19:14:07 +0800

    feat: implement falsh attention triton kernel

---
+++ cs336_systems/flash_attn_triton.py
@@ -0,0 +1,115 @@
+import triton
+import triton.language as tl	
+
+@triton.jit
+def flash_fwd_kernel(
+	Q_ptr, K_ptr, V_ptr,
+	O_ptr, L_ptr,
+	stride_qb, stride_qq, stride_qd,
+	stride_kb, stride_kk, stride_kd,
+	stride_vb, stride_vk, stride_vd,
+	stride_ob, stride_oq, stride_od,
+	stride_lb, stride_lq,
+	N_QUERIES, N_KEYS,
+	scale,
+	D: tl.constexpr,
+	Q_TILE_SIZE: tl.constexpr,
+	K_TILE_SIZE: tl.constexpr,
+):
+	# Program indices
+	query_tile_index = tl.program_id(0)
+	batch_index = tl.program_id(1)
+
+	# Offset each pointer with corresponding batch index
+	# multiplied with the batch stride for each tensor
+	Q_block_ptr = tl.make_block_ptr(
+		Q_ptr + batch_index * stride_qb,
+		shape=(N_QUERIES, D),
+		strides=(stride_qq, stride_qd),
+		offsets=(query_tile_index * Q_TILE_SIZE, 0),
+		block_shape=(Q_TILE_SIZE, D),
+		order=(1, 0),
+	)
+
+	K_block_ptr = tl.make_block_ptr(
+		K_ptr + batch_index * stride_kb,
+		shape=(N_KEYS, D),
+		strides=(stride_kk, stride_kd),
+		offsets=(0, 0),
+		block_shape=(K_TILE_SIZE, D),
+		order=(1, 0),
+	)
+
+	V_block_ptr = tl.make_block_ptr(
+		V_ptr + batch_index * stride_vb,
+		shape=(N_KEYS, D),
+		strides=(stride_vk, stride_vd),
+		offsets=(0, 0),
+		block_shape=(K_TILE_SIZE, D),
+		order=(1, 0),
+	)
+
+	L_block_ptr = tl.make_block_ptr(
+		L_ptr + batch_index * stride_lb + query_tile_index * Q_TILE_SIZE,
+		shape=(N_QUERIES,),
+		strides=(stride_lq,),
+		offsets=(query_tile_index * Q_TILE_SIZE),
+		block_shape=(Q_TILE_SIZE,),
+		order=(0,),
+	)
+
+	O_block_ptr = tl.make_block_ptr(
+		O_ptr + batch_index * stride_ob + query_tile_index * K_TILE_SIZE,
+		shape=(N_QUERIES, D),
+		strides=(stride_oq, stride_od),
+		offsets=(query_tile_index * Q_TILE_SIZE),
+		block_shape=(Q_TILE_SIZE, D),
+		order=(1, 0),
+	)
+
+	# On chip buffer 
+	Oi = tl.zeros((Q_TILE_SIZE, D), dtype=tl.float32)
+	li = tl.zeros((Q_TILE_SIZE,), dtype=tl.float32)
+	mi = tl.full((Q_TILE_SIZE,), float('-inf'), dtype=tl.float32)
+
+	Qi = tl.load(Q_block_ptr, boundary_check=(0,1), padding_option='zero')
+
+	for i in range(tl.cdiv(D, K_TILE_SIZE)):
+		Ki = tl.load(K_block_ptr, boundary_check=(0,1), padding_option='zero')
+		Vi = tl.load(V_block_ptr, boundary_check=(0, 1), padding_option='zero')
+		
+		# Compute tile of pre-softmax attention socres
+		Si = tl.zeros((Q_TILE_SIZE, K_TILE_SIZE), dtype=tl.float32)
+		Si = tl.dot(Qi, tl.trans(Ki), acc=Si) / scale
+		rows_max = tl.max(Si, axis=1)
+		
+		# Compute tile of attention scores row maximum and store in m_cur 
+		m_cur = tl.maximum(mi, rows_max)
+		facotr = tl.exp(mi - m_cur)
+		mi = m_cur
+
+		Pi = tl.exp(Si - mi[:,None])
+		li = factor * li + tl.sum(Pi, axis=1)
+
+		Oi = facotr * Oi
+		# Align data type of Pi to Vi
+		Pi = Pi.to(V_block_ptr.type.element_ty)
+		Oi = tl.dot(Pi, Vi, acc=Oi)
+	
+	# Do the outer scale
+	Oi = Oi / li[:, None]
+	li = mi + tl.log(li)
+
+	Oi = Oi.to(O_block_ptr.type.element_ty)
+	li = li.to(L_block_ptr.type.element_ty)
+	tl.store(O_block_ptr, Oi, boundary_check=(0, 1))
+	tl.store(L_block_ptr, li, boundary_check=(0,))
+
+	# Move the pointer to next tile along N_QUERIES
+	Q_block_ptr = Q_block_ptr.advance((Q_TILE_SIZE, 0))
+	O_block_ptr = O_block_ptr.advance((Q_TILE_SIZE, 0))
+
+	L_block_ptr = L_block_ptr.advance((Q_TILE_SIZE,))
+
+
+		
